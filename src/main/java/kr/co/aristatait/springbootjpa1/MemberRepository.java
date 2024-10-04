b6b588c4a2f6d200e070cd0e30f3af34e782f630
장성민 <aristatait@gmail.com>
2024-10-04 17:53:42 +0900

    init

---
+++ src/main/java/kr/co/aristatait/springbootjpa1/MemberRepository.java
@@ -0,0 +1,14 @@
+package kr.co.aristatait.springbootjpa1;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class MemberRepository {
+
+    @PersistenceContext
+    private EntityManager em;
+
+
+}
