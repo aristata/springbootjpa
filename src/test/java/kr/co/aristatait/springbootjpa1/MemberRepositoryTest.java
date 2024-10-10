47cef565341e91412d64cc62b16a1dbabe5c0420
장성민 <aristatait@gmail.com>
2024-10-10 17:44:13 +0900

    refactor: junit4 -> junit5 로 수정

--- src/test/java/kr/co/aristatait/springbootjpa1/MemberRepositoryTest.java
+++ src/test/java/kr/co/aristatait/springbootjpa1/MemberRepositoryTest.java
@@ -1,15 +1,15 @@
 package kr.co.aristatait.springbootjpa1;
 
-import org.assertj.core.api.Assertions;
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.Rollback;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.transaction.annotation.Transactional;
 
-@RunWith(SpringRunner.class)
+@ExtendWith(SpringExtension.class)
 @SpringBootTest
 public class MemberRepositoryTest {
 
@@ -28,9 +28,9 @@ public class MemberRepositoryTest {
         Member findMember = memberRepository.find(saveId);
 
         //then
-        Assertions.assertThat(findMember.getId()).isEqualTo(member.getId());
-        Assertions.assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
-        Assertions.assertThat(findMember).isEqualTo(member);
+        Assertions.assertEquals(findMember.getId(), member.getId());
+        Assertions.assertEquals(findMember.getUsername(), member.getUsername());
+        Assertions.assertEquals(findMember, member);
         System.out.println("findMember == member: " + (findMember == member));
     }
 }
